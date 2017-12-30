c230dae00efc3ab65cdef003421f6a0aa466ea54
LFZ <lfz1994@qq.com>
2017-12-30 21:25:44 +0800

    match feature in frame get frame canny

--- frames_analyze.py
+++ frames_analyze.py
@@ -1,21 +1,50 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 18 16:33:15 2014
-@author: duan
-"""
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-img_rgb = cv2.imread('test pic/1.jpg')
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('test pic/feature.png',0)
-w, h = template.shape[::-1]
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-threshold = 0.8
-#umpy.where(condition[, x, y])
-#Return elements, either from x or y, depending on condition.
-#If only condition is given, return condition.nonzero().
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-cv2.imwrite('res.png',img_rgb)
+
+img = cv2.imread('test pic/3.jpg')
+img_copy = img.copy()
+feature = cv2.imread('test pic/feature1.png')
+h,w,_ = feature.shape
+
+#cv2.imshow('img',img)
+#cv2.imshow('feature',feature)
+print w,h
+#re-size image to fit feature
+imgshape = img_copy.shape
+factor = imgshape[0]/640
+img = cv2.resize(img_copy,(imgshape[1]/factor,imgshape[0]/factor),interpolation=cv2.INTER_CUBIC)
+img_copy = img.copy()
+#find feature in frame
+res = cv2.matchTemplate(img,feature,cv2.TM_CCOEFF)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+top_left = max_loc
+bottom_right = (top_left[0] + w, top_left[1] + h)
+
+
+#find land box in frame
+img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(img_gray,127,255,0)
+image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+img = cv2.drawContours(img, contours, 3, (0,255,0), 3)
+img_canny = cv2.Canny(img_gray,100,150,apertureSize = 5)
+circles = cv2.HoughCircles(img_canny,cv2.HOUGH_GRADIENT,1,20,
+param1=50,param2=30,minRadius=60,maxRadius=100)
+print circles
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+
+img = cv2.drawContours(img, contours, 3, (0,255,0), 3)
+cv2.imshow('canny',img_canny)
+cv2.imshow('img_gray',img_gray)
+cv2.rectangle(img,top_left, bottom_right, 255, 2)
+cv2.imshow('result',img)
+
+#cv2.imwrite('test pic/canny.jpg',img_canny)
+
+
+cv2.waitKey(10000000)
+cv2.destroyAllWindows()
